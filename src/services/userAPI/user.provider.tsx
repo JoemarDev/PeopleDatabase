914038252ee6c291cbc193f947b2db238007a060
joemardev <joemarb.dev@gmail.com>
2023-10-18 21:18:19 +0800

    Add Comments in the context provider functions

--- src/services/userAPI/user.provider.tsx
+++ src/services/userAPI/user.provider.tsx
@@ -35,45 +35,87 @@ export const PeopleProvider = ({ children }: PeopleProviderProps) => {
 	const [page, setPage] = useState(1);
 
 	const restoreDeletedProfile = (profile: UserState) => {
+		// Reset the expiration and isSaved properties
 		profile.expiration = null;
 		profile.isSaved = true;
+
+		// Destructure the name property from the profile object
 		const { name: x } = profile;
 
+		// Filter the savedProfile array based on the first and last names
 		const filteredSavedProfile = savedProfile.filter(({ name: y }) => y.first !== x.first && y.last !== x.last);
+
+		// Add the restored profile to the filteredSavedProfile array
 		filteredSavedProfile.push(profile);
+
+		// Update the savedProfile state with the filteredSavedProfile array
 		setSavedProfile(filteredSavedProfile);
+
+		// Save the restored profile to the people profile storage
 		savePeopleProfile(profile);
 
+		// Filter the profileBin array based on the first and last names
 		const filteredBin = profileBin.filter(({ name: y }) => y.first !== x.first && y.last !== x.last);
+
+		// Update the profileBin state with the filteredBin array
 		setProfileBin(filteredBin);
+
+		// Update the profileBin in the local storage
 		window.localStorage.setItem("profileBin", JSON.stringify(filteredBin));
 	};
 
 	const processProfileBin = (profile: UserState) => {
+		// Get the current time
 		const currentTime = new Date();
+
+		// Calculate the expiration time as 5 minutes from the current time
 		const expirationTime = new Date(currentTime.getTime() + 5 * 60000);
+
+		// Set the expiration property of the profile to the calculated expiration time
 		profile.expiration = expirationTime;
+
+		// Destructure the name property from the profile object
 		const { name: x } = profile;
+
+		// Filter the profileBin array based on the first and last names
 		const filteredBin = profileBin.filter(({ name: y }) => y.first !== x.first && y.last !== x.last);
+
+		// Add the processed profile to the filteredBin array
 		filteredBin.push(profile);
+
+		// Update the profileBin state with the filteredBin array
 		setProfileBin(filteredBin);
+
+		// Update the profileBin in the local storage
 		window.localStorage.setItem("profileBin", JSON.stringify(filteredBin));
 	};
 
 	const removeFromProfileBin = (profile: UserState) => {
+		// Destructure the name property from the profile object
 		const { name: x } = profile;
+
+		// Filter the profileBin array based on the first and last names
 		const filteredBin = profileBin.filter(({ name: y }) => y.first !== x.first && y.last !== x.last);
+
+		// Update the profileBin state with the filteredBin array
 		setProfileBin(filteredBin);
+
+		// Update the profileBin in the local storage
 		window.localStorage.setItem("profileBin", JSON.stringify(filteredBin));
 	};
 
 	const prepareData = (dirtyData: UserState[]) => {
+		// Create an empty array for storing the prepared data
 		const dataPrep = [];
 
+		// Loop through the dirtyData array
 		for (let x = 0; x < dirtyData.length; x++) {
+			// Check if the "login" property exists in the current object
 			if ("login" in dirtyData[x]) {
+				// Destructure the necessary properties from the dirtyData object
 				const { gender, name, location, email, dob, registered, phone, cell, picture } = dirtyData[x];
 
+				// Prepare the location object with specific properties
 				const locationPrep = {
 					street: location.street,
 					city: location.city,
@@ -81,6 +123,7 @@ export const PeopleProvider = ({ children }: PeopleProviderProps) => {
 					country: location.country,
 				};
 
+				// Prepare the individual data object with desired properties
 				const prep = {
 					gender: gender,
 					name: name,
@@ -95,89 +138,153 @@ export const PeopleProvider = ({ children }: PeopleProviderProps) => {
 					expiration: null,
 				};
 
+				// Push the prepared object to the dataPrep array
 				dataPrep.push(prep);
 			}
 		}
 
+		// Return the prepared data array
 		return dataPrep;
 	};
 
 	const getPeopleLists = async (page = 1) => {
+		// Clear the existing people state and set isLoading to true
 		setPeopleState([]);
 		setIsLoading(true);
+
+		// Make an API call to fetch the people lists
 		const response = await axios.get<PeopleListsResponse>(
 			`https://randomuser.me/api/?page=${page}&results=9&seed=abc`,
 		);
 		const { results } = response.data;
 
+		// Prepare the fetched data
 		const dataPrep = prepareData(results);
 
+		// Update the people state with the prepared data and finish the loading
 		setPeopleState(dataPrep);
 		setIsLoading(false);
 		setPage(page);
+
+		// Check if there is any saved profile and update the state accordingly
 		checkSavedProfile(savedProfile, dataPrep);
+
+		// Return the prepared data
 		return dataPrep;
 	};
 
 	const loadSavedProfile = (): UserState[] => {
+		// Get the current saved profiles from local storage
 		const currentSaved = window.localStorage.getItem("profiles");
 
 		let parsedData: UserState[] = [];
 
-		if (currentSaved) parsedData = JSON.parse(currentSaved);
+		// If there are saved profiles, parse the data
+		if (currentSaved) {
+			parsedData = JSON.parse(currentSaved);
+		}
 
+		// Return the parsed data
 		return parsedData;
 	};
 
 	const savePeopleProfile = (profile: UserState) => {
+		// Load the saved profiles from local storage
 		const parsedData: UserState[] = loadSavedProfile();
+
+		// Set the isSaved property of the profile to true
 		profile.isSaved = true;
+
+		// Add the profile to the parsed data array
 		parsedData.push(profile);
+
+		// Save the updated parsed data back to local storage
 		window.localStorage.setItem("profiles", JSON.stringify(parsedData));
+
+		// Update the state with the saved profiles
 		setSavedProfile(parsedData);
 	};
 
 	const unSavedProfile = (profile: UserState) => {
+		// Destructure the name property from the profile object
 		const { name: y } = profile;
+
+		// Load the saved profiles from local storage
 		let parsedData: UserState[] = loadSavedProfile();
+
+		// Filter out the profile that matches the first and last name
 		parsedData = parsedData.filter(({ name: x }) => x.first !== y.first && x.last !== y.last);
+
+		// Save the updated parsed data back to local storage
 		window.localStorage.setItem("profiles", JSON.stringify(parsedData));
+
+		// Update the state with the saved profiles
 		setSavedProfile(parsedData);
+
+		// Check if the profile is present in peopleState and remove it if found
 		checkSavedProfile(parsedData, peopleState);
+
+		// Process the profile bin for the removed profile
 		processProfileBin(profile);
 	};
 
 	const checkSavedProfile = (savedProfileData: UserState[], fetchProfile: UserState[]) => {
+		// Create a clone of the data from the API results and saved data
 		const ClonedState = [...fetchProfile];
+
+		// Iterate over each item in the cloned state
 		ClonedState.map((item) => {
+			// Destructure the name property from the item object
 			const { name: y } = item;
+
+			// Find a matching profile in the saved profile data
 			const matchProfile = savedProfileData.find(({ name: x }) => x.first === y.first && x.last === y.last);
+
+			// If a match is found, set the isSaved property to true, otherwise set it to false
 			if (matchProfile) {
 				item.isSaved = true;
 			} else {
 				item.isSaved = false;
 			}
 		});
+
+		// Update the state with the modified ClonedState
 		setPeopleState(ClonedState);
 	};
 
 	const LoadAndFetch = async (page = 1) => {
+		// Fetch people lists from API
 		const fetchResults = await getPeopleLists(page);
+
+		// Load saved profile data
 		const parsedData: UserState[] = loadSavedProfile();
+
+		// Set the saved profile state
 		setSavedProfile(parsedData);
+
+		// Check and update the saved status for each fetched profile
 		checkSavedProfile(parsedData, fetchResults);
 	};
 
 	const loadLocalProfile = () => {
+		// Load saved profile data
 		const parsedData: UserState[] = loadSavedProfile();
+
+		// Set the saved profile state
 		setSavedProfile(parsedData);
 	};
 
 	const loadProfileBinStorage = () => {
+		// Retrieve the current profile bin item from local storage
 		const currentBinItem = window.localStorage.getItem("profileBin");
+
+		// Initialize an empty array to store the parsed data
 		let parsedData: UserState[] = [];
+
+		// Parse the current bin item if it exists
 		if (currentBinItem) parsedData = JSON.parse(currentBinItem);
 
+		// Filter out expired items from the parsed data
 		const filterData = parsedData.filter((x) => {
 			const { expiration } = x;
 			if (expiration) {
@@ -187,6 +294,7 @@ export const PeopleProvider = ({ children }: PeopleProviderProps) => {
 			}
 		});
 
+		// Set the filtered data as the profile bin state
 		setProfileBin(filterData);
 	};
 
